a2247e35b6d519fdd1611d96414673413288172b
Dilip Kumar <94988616+Dilip070@users.noreply.github.com>
2024-05-12 13:23:15 +0530

    course management system using Java Spring Mysql

---
+++ course-management-system/src/main/java/io/javabrains/CourseDemoDataApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDemoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDemoDataApplication.class, args);
+	}
+
+}
